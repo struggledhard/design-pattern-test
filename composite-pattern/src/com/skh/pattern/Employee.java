a4a69feccd3999a704d5778c12531ce20b0f61c0
struggledhard <1224341153@qq.com>
2018-07-05 17:07:52 +0800

    组合模式学习

---
+++ composite-pattern/src/com/skh/pattern/Employee.java
@@ -0,0 +1,42 @@
+package com.skh.pattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by User: skh.
+ * Date: 2018/7/5 Time: 16:35.
+ * Description: 创建 Employee 类，该类带有 Employee 对象的列表。
+ */
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    //构造函数
+    public Employee(String name,String dept, int sal) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = sal;
+        subordinates = new ArrayList<>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates(){
+        return subordinates;
+    }
+
+    public String toString(){
+        return ("Employee :[ Name : "+ name
+                +", dept : "+ dept + ", salary :"
+                + salary+" ]");
+    }
+}
