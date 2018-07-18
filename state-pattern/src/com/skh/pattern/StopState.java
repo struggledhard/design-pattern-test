ae2c08e93b8b248ac11b29b9e73d32f99b7e83b5
struggledhard <1224341153@qq.com>
2018-07-18 15:01:12 +0800

    状态模式学习

--- state-pattern/src/com/skh/pattern/StopState.java
+++ state-pattern/src/com/skh/pattern/StopState.java
@@ -8,11 +8,11 @@ package com.skh.pattern;
 public class StopState implements State {
     @Override
     public void doAction(Context context) {
-        System.out.println("Player is in start state");
+        System.out.println("Player is in stop state");
         context.setState(this);
     }
 
     public String toString(){
-        return "Start State";
+        return "Stop State";
     }
 }
